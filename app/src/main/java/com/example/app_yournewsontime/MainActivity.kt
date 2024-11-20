10b7bde52ebb1912fd33b0d0b3f19991b40c2643
Nuhacet Travieso del Rosario <nuhacet66@gmail.com>
2024-11-20 12:34:00 +0000

    Integration the analytics code in the mainActivity

--- app/src/main/java/com/example/app_yournewsontime/MainActivity.kt
+++ app/src/main/java/com/example/app_yournewsontime/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.app_yournewsontime
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.app_yournewsontime.fragments.AuthActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("InvalidAnalyticsName")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Lanzamiento de eventos personalizados en nuestra aplicacion
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integracion de Firebase completa")
+        analytics.logEvent("Inicio de sesion",bundle)
+
+        // Configurar el listener para el botón de inicio
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // Encuentra el botón de inicio y configura el listener
+        val getStartedButton: Button = findViewById(R.id.get_started_button)
+        getStartedButton.setOnClickListener {
+            val bottomSheet = AuthActivity()
+            bottomSheet.show(supportFragmentManager, "AuthActivity")
         }
     }
 }
---
+++ app/src/main/java/com/example/app_yournewsontime/fragments/AuthActivity.kt
@@ -0,0 +1,10 @@
+package com.example.app_yournewsontime.fragments
+
+import com.example.app_yournewsontime.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AuthActivity() : BottomSheetDialogFragment() {
+
+
+
+}
