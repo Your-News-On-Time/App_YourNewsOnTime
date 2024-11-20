8ae15bf73ef48b04f8432757d458d272ba6fec3f
Nuhacet Travieso del Rosario <nuhacet66@gmail.com>
2024-11-20 13:57:36 +0000

    Create the buttoms and the funcionality for navegate beetween views

--- app/src/main/java/com/example/app_yournewsontime/MainActivity.kt
+++ app/src/main/java/com/example/app_yournewsontime/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.app_yournewsontime
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.app_yournewsontime.fragments.AuthActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
@@ -23,17 +23,11 @@ class MainActivity : AppCompatActivity() {
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("Inicio de sesion",bundle)
 
-        // Configurar el listener para el botón de inicio
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         // Encuentra el botón de inicio y configura el listener
         val getStartedButton: Button = findViewById(R.id.get_started_button)
         getStartedButton.setOnClickListener {
-            val bottomSheet = AuthActivity()
-            bottomSheet.show(supportFragmentManager, "AuthActivity")
+            val intent = Intent(this, AuthActivity::class.java)
+            setContentView(R.layout.activity_auth)
         }
     }
 }
