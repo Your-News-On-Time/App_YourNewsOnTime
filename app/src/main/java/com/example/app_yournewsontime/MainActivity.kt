c965196d31e28590830c913457407b812b0552b3
Nuhacet Travieso del Rosario <nuhacet66@gmail.com>
2024-11-24 18:15:27 +0000

    Implementation the verification Firebase  key

--- app/src/main/java/com/example/app_yournewsontime/MainActivity.kt
+++ app/src/main/java/com/example/app_yournewsontime/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
